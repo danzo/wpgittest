400acd06f8e19148e9e1776042559e342051b51b
Daniel Brasic <dbrasic@asu.edu>
2025-03-11 11:51:44 -0700

    updated config

--- wp-config copy.php
+++ wp-config copy.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'qxcpgetsys' );
+define( 'DB_NAME', 'wpgittest' );
 
 /** Database username */
-define( 'DB_USER', 'qxcpgetsys' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'X9JPZjpmRW' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
